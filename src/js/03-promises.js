44be55b3e3ff54b08954ebf1a6fe15fa6aad0cb3
Anastasia-Kostenko <nanochka03@gmail.com>
2023-12-01 20:39:11 +0200

    done_hw_09

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,30 @@
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const { delay, stop, amout } = event.currentTarget.elements;
+
+  setTimeout(() => {
+    for (let i = 1; i <= +amout.value; i++) {
+      createPromise(position, delay)
+    }
+  }, +delay.value)
+}
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        // Fulfill
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+        // Reject
+      }
+    }, delay)
+  })
 }
