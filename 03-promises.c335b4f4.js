f05e079fdaf8183162dfb14ff2de01757cf84f13
Anastasia-Kostenko <139360670+Anastasia-Kostenko@users.noreply.github.com>
2023-12-04 05:23:29 +0000

    Deploying to gh-pages from @ Anastasia-Kostenko/goit-js-hw-09@9a8ac70ed07ce186fdf97c41e34019cfe4526e28 🚀

--- 03-promises.c335b4f4.js
+++
@@ -1,2 +0,0 @@
-function e(e,t){return new Promise(((n,o)=>{setTimeout((()=>{Math.random()>.3?n(`✅ Fulfilled promise ${e} in ${t}ms`):o(`❌ Rejected promise ${e} in ${t}ms`)}),t)}))}document.querySelector(".form").addEventListener("submit",(function(t){t.preventDefault();const{delay:n,stop:o,amout:r}=t.currentTarget.elements;setTimeout((()=>{for(let t=1;t<=+r.value;t++)e(position,n)}),+n.value)}));
-//# sourceMappingURL=03-promises.c335b4f4.js.map
